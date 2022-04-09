234cde313dca8596cb6f6efab4ad338ca6458cc9
Ike Lyons <lyonsw@rpi.edu>
2022-04-08 20:38:43 -0400

    seperating spawn and init from main loop

--- main.mjs
+++ main.mjs
@@ -2,16 +2,34 @@ import { getObjectsByPrototype } from "/game/utils";
 import { Creep, StructureSpawn, Source, StructureContainer } from "/game/prototypes";
 import { MOVE, WORK, CARRY, ATTACK, RESOURCE_ENERGY, ERR_NOT_IN_RANGE } from "/game/constants";
 
-var worker;
+var workers = [];
 var attackCreeps = [];
+var mySpawn, enemySpawn;
+
+function spawn() {
+    if (workers.length < 2) {
+        var worker = mySpawn.spawnCreep([MOVE, WORK, CARRY]).object;
+        if (worker) {
+            workers.push(worker);
+        }
+    } else {
+        var attackCreep = mySpawn.spawnCreep([MOVE, ATTACK]).object;
+        if (attackCreep) {
+            attackCreeps.push(attackCreep);
+        }
+    }
+}
+
+function init() {
+    mySpawn = getObjectsByPrototype(StructureSpawn).find((i) => i.my);
+    enemySpawn = getObjectsByPrototype(StructureSpawn).find((i) => !i.my);
+}
 
 export function loop() {
-    var mySpawn = getObjectsByPrototype(StructureSpawn).find((i) => i.my);
-    var enemySpawn = getObjectsByPrototype(StructureSpawn).find((i) => !i.my);
+    init();
+    spawn();
 
-    if (!worker) {
-        worker = mySpawn.spawnCreep([MOVE, WORK, CARRY]).object;
-    } else {
+    for (var worker of workers) {
         if (worker.store.getFreeCapacity(RESOURCE_ENERGY)) {
             var container = worker.findClosestByPath(getObjectsByPrototype(StructureContainer));
             console.log(container);
@@ -23,21 +41,17 @@ export function loop() {
                 worker.moveTo(mySpawn);
             }
         }
+    }
 
-        var attackCreep = mySpawn.spawnCreep([MOVE, ATTACK]).object;
-        var enemies = getObjectsByPrototype(Creep).filter((creep) => !creep.my);
-        if (attackCreep) {
-            attackCreeps.push(attackCreep);
-        }
-        enemies.push(enemySpawn);
-        for (var creep of attackCreeps) {
-            // if (creep.attack(enemySpawn) == ERR_NOT_IN_RANGE) {
-            //     creep.moveTo(enemySpawn);
-            // }
-            var enemy = creep.findClosestByPath(enemies);
-            if (creep.attack(enemy) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(enemy);
-            }
+    var enemies = getObjectsByPrototype(Creep).filter((creep) => !creep.my);
+    enemies.push(enemySpawn);
+    for (var creep of attackCreeps) {
+        // if (creep.attack(enemySpawn) == ERR_NOT_IN_RANGE) {
+        //     creep.moveTo(enemySpawn);
+        // }
+        var enemy = creep.findClosestByPath(enemies);
+        if (creep.attack(enemy) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(enemy);
         }
     }
 }
