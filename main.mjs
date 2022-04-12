e032d438e3959dc1f98ed0f5a5836912a0d04083
Ike Lyons <lyonsw@rpi.edu>
2022-04-11 20:05:18 -0400

    Reintroduced healers

--- main.mjs
+++ main.mjs
@@ -6,6 +6,10 @@ var workers = [];
 var army = [];
 var currentSquad = [];
 var mySpawn, enemySpawn;
+var spawnDelay = 0;
+var squadSize = 4;
+var attackCreepBody = [MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK];
+var healCreepBody = [MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, HEAL];
 
 function spawn() {
     if (workers.length < 2) {
@@ -14,25 +18,17 @@ function spawn() {
             workers.push(worker);
         }
     } else {
-        if (currentSquad.length < 2) {
-            var attackCreep = mySpawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK]).object;
-            if (attackCreep) {
-                attackCreep.waitingForSquad = true;
-                currentSquad.push(attackCreep);
-                army.push(attackCreep);
-            }
-        } else {
-            // let attackCreep = mySpawn.spawnCreep([MOVE, HEAL]).object;
-            var attackCreep = mySpawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK]).object;
-
-            if (attackCreep) {
-                for (var c of currentSquad) {
-                    c.waitingForSquad = false;
-                }
-                attackCreep.waitingForSquad = false;
-                army.push(attackCreep);
-                currentSquad = [];
-            }
+        var attackCreep;
+        if (currentSquad.length < squadSize - 1) {
+            attackCreep = mySpawn.spawnCreep(attackCreepBody).object;
+        } else if (currentSquad.length < squadSize) {
+            attackCreep = mySpawn.spawnCreep(healCreepBody).object;
+        }
+        if (attackCreep) {
+            spawnDelay = attackCreepBody.length * 3;
+            attackCreep.waitingForSquad = true;
+            currentSquad.push(attackCreep);
+            army.push(attackCreep);
         }
     }
 }
@@ -41,6 +37,16 @@ function init() {
     mySpawn = getObjectsByPrototype(StructureSpawn).find((i) => i.my);
     enemySpawn = getObjectsByPrototype(StructureSpawn).find((i) => !i.my);
     army = army.filter((creep) => creep.exists);
+    if (currentSquad.length >= squadSize) {
+        if (spawnDelay > 0) {
+            spawnDelay--;
+            return;
+        }
+        for (var creep of currentSquad) {
+            creep.waitingForSquad = false;
+        }
+        currentSquad = [];
+    }
 }
 
 export function loop() {
@@ -66,13 +72,6 @@ export function loop() {
         if (creep.waitingForSquad) {
             continue;
         }
-        if (creep.body.some((bp) => bp.type == ATTACK)) {
-            var enemy = creep.findClosestByPath(enemies);
-            if (creep.attack(enemy) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(enemy);
-            }
-        }
-
         if (creep.body.some((bp) => bp.type == HEAL)) {
             var myDamagedCreeps = army.filter((i) => i.hits < i.hitsMax);
             var healTarget = creep.findClosestByPath(myDamagedCreeps);
@@ -82,6 +81,12 @@ export function loop() {
                 }
             } else {
                 var enemy = creep.findClosestByPath(enemies);
+                creep.moveTo(enemy);
+            }
+        }
+        if (creep.body.some((bp) => bp.type == ATTACK)) {
+            var enemy = creep.findClosestByPath(enemies);
+            if (creep.attack(enemy) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(enemy);
             }
         }
